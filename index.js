617f4b909b1fe10f4ef1a99c42274d6b797b1169
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-02-17 11:37:31 +0000

    Have installed the mysql package for Node.js
    Have installed the mysql package for the Node.js API for the application. mysql dependencies have been added to the "package.json" file.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const server = app.listen(8080);
 
+// EJS app view engine: for express api
 app.set("view engine", "ejs");
 app.use("/assets", express.static("assets"));
+
+// Creating connection to MySQL DB Schema
 
 app.get("/", (req, res) => {
     res.render("index");
