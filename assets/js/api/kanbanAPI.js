1732e28f6db3703ccd78082968109fcc672a58a2
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-03-17 10:02:40 +0000

    Have coded the Kanban data storage configuration.
    Have coded the following functions within the KanbanAPI js class: "getItems, insertItem, updateItem & deleteItem". These four key functions deal with the CRUD functionality of task card data being modified on the kanban-board web page. The "updateItem" function also contains the updating of the position for the task card when it is dropped into another Kanban column on the web page. This uses the items.splice method which can move the array of data content within that task card to another column.
    
    The "read" function gets the data items from the data key "board-data" and returns each kanban column as a JSON array. Each column/array ID is numbered from 1-3.
    
    The "save" function sets the "board-data" storage key to it's updated data when text content within a task card is changed. This uses the JSON.stringify for data.

--- assets/js/api/kanbanAPI.js
+++ assets/js/api/kanbanAPI.js
@@ -1,32 +1,111 @@
 export default class kanbanAPI {
     static getItems(columnId) {
         const column = read().find(column => column.id == columnId);
+
+        if (!column) {
+            return [];
+        }
+
+        return column.items;
+    }
+
+    static insertItem(columnId, content) {
+        const data = read();
+        const column = data.find(column => column.id == columnId);
+        const item = {
+            id: Math.floor(Math.random() * 1000000),
+            content
+        };
+
+        if(!column) {
+            console.error("There is no Column");
+        }
+
+        column.items.push(item);
+        save(data);
+
+        return item;
+    }
+
+    // GO BACK TO THIS FUNCTION
+    static updateItem(itemId, newData) {
+        const data = read();
+        const [item, currentColumn] = (() => {
+            for (const column of data) {
+                const item = column.items.find(item => item.id == itemId);
+
+                if (item) {
+                    return [item, column];
+                }
+            }
+        })();
+
+        if (!item) {
+            console.error("Item not found.");
+        }
+
+        item.column = newData.content == undefined ? item.content : newData.content;
+
+        // Updating the column and position of the task card.
+        if (
+            newData.columnId !== undefined
+            && newData.position !== undefined
+        ) {
+            const selectedColumn = data.find(column => column.id == newData.columnId);
+
+            if (!selectedColumn) {
+                console.error("Selected column not found.");
+            }
+
+            // Delete the task card from it's current column.
+            currentColumn.items.splice(currentColumn.items.indexOf(item), 1);
+
+            // Moving task card into it's new selected column & position.
+            selectedColumn.items.splice(newData.position, 0, item);
+        }
+
+        save(data);
+    }
+
+    static deleteItem(itemId) {
+        const data = read();
+
+        for (const column of data) {
+            const item = column.items.find(item => item.id == itemId);
+
+            if (item) {
+                column.items.splice(column.items.indexOf(item), 1);
+            }
+        }
+
+        save(data);
     }
 }
 
+// read function
 function read() {
     const json = localStorage.getItem("board-data");
     
     if (!json) {
-    return [
-        {
-            id: 1,
-            items: []
-        },
-        {
-            id: 2,
-            items: []
-        },
-        {
-            id: 3,
-            items: []
-        }
+        return [
+            {
+                id: 1,
+                items: []
+            },
+            {
+                id: 2,
+                items: []
+            },
+            {
+                id: 3,
+                items: []
+            }
         ];
     }
 
     return JSON.parse(json);
 }
-
+// save function
 function save(data) {
     localStorage.setItem("board-data", JSON.stringify(data));
 }
