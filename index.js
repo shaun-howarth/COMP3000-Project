553e993297d5d43416699398e19cabc4adc7021a
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-04-18 15:31:37 +0100

    Have started to code SQL database queries (API endpoints).
    Within the index.js file, two API endpoint functions for the express API have been coded so far.
    
    These include the app.get function that contains the MySQL query for selecting all records from the "personnel" table in the schema where only the "active" personnel users are to be displayed. The "rows" object is used as a parameter to store and pass through the database query. This rows object is then declared within the "home-table.hbs" web page using the  {{#each rows}} {{/each}} tag setup. In-between these tags is the HTML tables <tr> and <td> tags so a records of data containing the correct column values for data can be rendered and pushed through from the database table.
    
    The second API endpoint function that has been coded so far is the app.post function that contains the MySQL query for searching for specific users (records of data) from the table when using the search input field. So far this has only been enabled by the these two table attribute names which are first_name & last_name. These two fields used '%' + searchRecord + '%'   where searchRecord is the name of the JS variable used to store the req.body.search. ".search" is the class name in the HTML for the input form where string text is entered and the API can communicate with the database to pull those search results form the table.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const res = require("express/lib/response");
 dotenv.config({ path: './.env'});
 
 app.listen(4000, () => {
-    console.log("Server started on Port 4000")
+    console.log("Server started on Port 4000");
 });
 
 let engines = require("consolidate");
@@ -36,23 +36,24 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-// creating connection string to MySQL DB Schema: Local instance
+// creating connection pool to MySQL DB Schema: Local instance
+const db = mysql.createConnection({
+    host: process.env["DATABASE_HOST"],
+    user: process.env["DATABASE_USER"],
+    password: process.env["DATABASE_PASSWORD"],
+    database: process.env["DATABASE"]
+}); 
 
-//const db = mysql.createConnection({
-    //host: process.env["DATABASE_HOST"],
-    //user: process.env["DATABASE_USER"],
-    //password: process.env["DATABASE_PASSWORD"],
-    //database: process.env["DATABASE"]
-//}) 
+//db getConnection method to MySQL environment
+db.connect((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+    console.log('Connected to MySQL Database');
+    }
+});
 
-//db connect method to MySQL environment
-//db.connect((err) => {
-    //if(err) {
-        //console.log(err);
-   // } //else {
-    //console.log('Connected to MySQL Database');
-    //}
-//});
+
 
 
 // ejs render page routes
@@ -81,6 +82,39 @@ app.get("/user-sign-up" , (req, res) => {
 });
 
 // handlebars (hbs) page routes
+//const routes = require("./server/routes/personnel");
+//app.use("/home-table", routes);
+
+
+// view user records from personnel db table
 app.get("/home-table", (req, res) => {
-    res.render("home-table.hbs");
+    // MySQL query select statement grabbing to view active users only with WHERE clause
+    db.query('SELECT * FROM personnel WHERE status ="active"', (err, rows) => {
+
+        if(!err) {
+            res.render("home-table.hbs", { rows });
+        } else {
+            console.log(err);
+        }
+        console.log("Data records from personnel table: \n",rows);
+    });
 });
+
+// finding personnel user by seacrh
+app.post("/home-table", (req, res) => {
+
+    let searchRecord = req.body.search;
+
+    // MySQL query
+    db.query('SELECT * FROM personnel WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchRecord + '%', '%' + searchRecord + '%'], (err, rows) => {
+
+        if(!err) {
+            res.render("home-table.hbs", { rows });
+        } else {
+            console.log(err);
+        }
+        console.log("Data records from personnel table: \n",rows);
+    });
+
+});
+
